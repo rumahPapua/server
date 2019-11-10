21b80ac63565dd342e77a81d4a4486985aa221cc
hilarion <hilarion_cross@hotmail.com>
2019-11-11 01:27:30 +0700

    setup es6

---
+++ src/app.js
@@ -0,0 +1,31 @@
+// app.js
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+require('dotenv').config()
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import indexRouter from './routes/index';
+import mongoose from 'mongoose'
+
+const app = express();
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('rumah papua app connected')
+});
+
+var mongodUri = `mongodb://${process.env.MLAB_USER}:${process.env.MLAB_PASS}${process.env.MLAB_URL}`
+mongoose.connect(mongodUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../public')));
+app.use('/', indexRouter);
+export default app;
